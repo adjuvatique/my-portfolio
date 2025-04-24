f62d64f3dc70a13e5b866ee56013032fc831ed8d
Aleksandrs <a.bilanins@yahoo.com>
2025-04-24 13:07:56 +0100

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // Если ваш Pages-URL — https://adjuvatique.github.io/my-portfolio/
   base: '/my-portfolio/',
   plugins: [react()],
-})
+});
